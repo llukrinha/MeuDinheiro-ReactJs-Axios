778d0569e427f6a51e6c932ee13409f2e703fff8
Lucas Cantanhêde <lucas.cantanhede>
2021-03-24 12:08:05 -0300

    Implementando com Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from 'react'
+
+import Rest from './rest'
+
+const baseURL = 'https://meudinheiro-hotmart-default-rtdb.firebaseio.com/'
+const {useGet, usePost, useDelete} = Rest(baseURL)
 
 function App() {
+    const data = useGet('movimentacoes/2019-08')
+    const [postData, post] = usePost('movimentacoes/2019-08')
+    const [deleteData, remove] = useDelete()
+
+    const saveNew = () => {
+        post({valor: 10, descricao: 'Olá'})
+    }
+    const doRemove = () => {
+        remove('movimentacoes/2019-08/')
+    }
     return (
-        <div className='App'>
-            <h1>Minhas Series</h1>
+        <div>
+            <nav className='navbar navbar-light bg-light'>
+                <div className='container'>
+                    <a className='navbar-brand'>Meu dinheiro</a>
+                </div>
+            </nav>
+            {JSON.stringify(data)}
+            {data.loading && <p>Loading..</p>}
+            <button onClick={saveNew}>Salvar</button>
+            <pre>  {JSON.stringify(postData)}</pre>
+            <button onClick={doRemove}>Delete</button>
+            <pre>{JSON.stringify(deleteData)}</pre>
+
         </div>
     );
 }
---
+++ src/rest.js
@@ -0,0 +1,80 @@
+import {useReducer, useEffect} from 'react'
+import axios from "axios";
+
+const INITIAL_STATE = {
+    loading: true,
+    data: {}
+}
+const reducer = (state, action) => {
+    if (action.type === 'REQUEST') {
+        return {
+            ...state,
+            loading: true
+        }
+    }
+    if (action.type === 'SUCCESS') {
+        return {
+            ...state,
+            loading: false,
+            data: action.data
+        }
+    }
+    return state
+}
+
+const init = baseURL => {
+    const useGet = resource => {
+        let data, dispatch;
+        [data, dispatch] = useReducer(reducer, INITIAL_STATE);
+        useEffect(() => {
+            dispatch({type: 'REQUEST'})
+            axios
+                .get(baseURL + resource + '.json')
+                .then(r => {
+                    dispatch({type: 'SUCCESS', data: r.data})
+                })
+        }, [])
+        return data
+    }
+
+    const usePost = resource => {
+        let data, dispatch;
+        [data, dispatch] = useReducer(reducer, INITIAL_STATE);
+        const post = data => {
+            dispatch({type: 'REQUEST'})
+            axios
+                .post(baseURL + resource + '.json', data)
+                .then(r => {
+                    dispatch({
+                        type: 'SUCCESS',
+                        data: r.data
+                    })
+                })
+        }
+        return [data, post]
+    }
+
+    const useDelete = () => {
+        let data, dispatch;
+        [data, dispatch] = useReducer(reducer, INITIAL_STATE)
+        const remove = resource => {
+            dispatch({type: 'REQUEST'})
+            axios
+                .delete(baseURL + resource + '.json')
+                .then(() => {
+                    dispatch({
+                        type: 'SUCCESS',
+                    })
+                })
+        }
+        return [data, remove]
+    }
+
+    return {
+        useGet,
+        usePost,
+        useDelete
+    }
+}
+
+export default init;
